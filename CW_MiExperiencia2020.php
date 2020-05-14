740ccdb938aa9e3bd5d090047ddb7940b32e5002
CristianUmAg <cristianu@comunidad.unam.mx>
2020-05-14 18:45:20 -0500

    Feat: Añadi un archivo php con mi experiencia en el cw2020

---
+++ CW_MiExperiencia2020.php
@@ -0,0 +1,11 @@
+
+Mi nombre es: Umaña Aguirre Cristian Alberto :)
+De este curso he aprendido: A sufrir xd, de una manera "positiva", pues gracias a esto me he presionado para pensar de
+distintas maneras, tal vez unas más convencionales que otras, pero todo con el fin de encontrar la respuesta a los diferentes problemas;
+también he aprendido a no quedarme cerrado en una solucion pese a que ya puncione, pues varias veces he llegado a un punto en
+el que las cosas parecen no tener "avance", pero viendolo desde otra perspectiva o empezando de cero y tomando en cuenta
+mis errores previos, descubro que me estaba complicando mucho y que había una manera más sencilla de hacerlo; por lo que eso me da una nueva
+vision sobre los asuntos en general, el hecho de no tener un pensamiento tan plano por así decirlo. :)
+Creo poder agregar que espero con ansias los modulos de diseño :)
+
+*Nota: Me equivoque en el nombre del repositor :( una disculpa :)
